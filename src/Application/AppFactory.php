d84ce89618dfaee188a6bf4680cdcaa32dbe9f23
Kcchouette <kcchouette@posteo.org>
2026-06-14 12:58:55 +0200

    fix: load .env file in AppFactory, add .env.example

--- src/Application/AppFactory.php
+++ src/Application/AppFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Orchestrator\Application;
 
 use DI\ContainerBuilder;
+use Dotenv\Dotenv;
 use Slim\App;
 use Slim\Factory\AppFactory as SlimAppFactory;
 
@@ -12,6 +13,8 @@ class AppFactory
 {
     public function create(): App
     {
+        $this->loadEnv();
+
         $containerBuilder = new ContainerBuilder();
         $containerBuilder->addDefinitions(__DIR__ . '/../../config/dependencies.php');
         $container = $containerBuilder->build();
@@ -27,5 +30,21 @@ class AppFactory
         require __DIR__ . '/routes.php';
 
         return $app;
+    }
+
+    private function loadEnv(): void
+    {
+        $paths = [
+            __DIR__ . '/../../.env',
+            '/etc/fusiondirectory-orchestrator/orchestrator.conf',
+        ];
+
+        foreach ($paths as $path) {
+            if (file_exists($path)) {
+                $dotenv = Dotenv::createImmutable(dirname($path));
+                $dotenv->load();
+                return;
+            }
+        }
     }
 }
